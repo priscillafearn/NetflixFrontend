33206f29ce57fc88a2386df0f38ba8595b4534d0
Priscilla Fearn <fearn.priscilla@gmail.com>
2021-06-14 18:53:49 +0100

    updates for streamlite

--- app.py
+++ app.py
@@ -36,36 +36,34 @@ if view == page_list[0]:
     # put fetaures in the params dictionary
     params = {"year":response["Year"],
               "rated":response["Rated"],
-    #          "Released":"19 Dec 1997",
+              "Released":response["Released"],
               "runtime":response["Runtime"],
-    #          "Genre":"Drama, Romance",
-    #          "Director":"James Cameron",
-    #          "Writer":"James Cameron",
-    #          "Actors":"Leonardo DiCaprio",
-    #          "Language":"English, French",
+   #           "Genre":"Drama, Romance",
+              "Director":response["Director"],
+              "Writer": response["Writer"],
+              "Actors": response["Actors"],
+              "Language":response["Language"],
               "country":response["Country"]}
     
     # response
-    response = requests.get(url, params = params).json()
+    # response = requests.get(url, params = params).json()
     # st.write(response)
     st.write(f'Title: ', response["Title"])
     st.write(f'Year of release: ', response["Year"])
     st.write(f'Length of movie: ', response["Runtime"])
     st.write(f'Country of origin: ', response["Country"])
+    st.write(f'Director: ', response["Director"])
+    st.write(f'Actor: ', response["Actors"])
+    st.write(f'Language: ', response["Language"])
+    st.write(f'PG Rating: ', response["Rated"])
+
+    
+
 
     # call our IMDB API with param dictinary made from IMDB response 
-    url_model = "http://127.0.0.1:8000/predict?"
-    response = requests.get(url_model, params = params).json()
+    url = "http://127.0.0.1:8000/predict?"
+    response = requests.get(url, params = params).json()
     st.write(response)
-# get request using movie_titel
-
-# # if(st.button("Submit")):
-    #     result = movie_title.title()
-    #     st.success(result)
-        
-    params = dict(
-    movie_title=movie_title,
-    )
 
 
 # below is the code for the other page_list -> Build Your Own
@@ -75,36 +73,43 @@ elif view == page_list[1]:
     
     #vadd here input information
     #year = st.text_input('Year of movie release')
-    movie_title = st.text_input('Input movie name')
-    movie_title = st.text_input('Input movie name')
-    movie_title = st.text_input('Input movie name')
-    movie_title = st.text_input('Input movie name')
-    movie_title = st.text_input('Input movie name')
-    
-    
-    
+    # movie_title = st.text_input('Input movie name')
+    # movie_title = st.text_input('Input movie name')
+    # movie_title = st.text_input('Input movie name')
+    # movie_title = st.text_input('Input movie name')
+    # movie_title = st.text_input('Input movie name')
     
     
     year = st.slider("Year of movie release", min_value = 1950, max_value= 2022, step=1)
     runtime = st.slider('Total runtime of movie in minutes', min_value = 30, max_value = 500, step = 5)
     rated = st.selectbox('PG Rating', options = ["PG13", "teens", "kids"])
     country = st.selectbox('Is this an American movie?', options = ["Yes", "No"])
+    language = st.text_input('Input language here')
+    released = st.text_input('Input month of released here')
+    writer = st.text_input('Input name of writer here')
+    director = st.text_input('Input name of director here')
     
-    params = dict(
+   
+    params_model = dict(
     year=year,
     runtime=runtime,
     rated=rated,
     country=country,
+    language=language,
+    released=released,
+    writer=writer,
+    director=director
     )
     
-    
+ 
     # enter here the address of your flask api
-    url = 'https://????/predict'
+   
 
+    url_model = "http://127.0.0.1:8000/predict?"
+    
+    response = requests.get(url_model, params=params_model()).json()
 
-    response = requests.get(url, params=params)
-
-    prediction = response.json()
+    prediction = response
 
     pred = prediction['prediction']
 
